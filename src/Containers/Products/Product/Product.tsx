789db8596cc49f8db79f8c7c212fa4d20828a9b9
RyDVi <dmitrii.kardan.rch@mail.ru>
2020-05-25 01:28:17 +0300

    optimized site. Added favicon.ico

--- src/Containers/Products/Product/Product.tsx
+++ src/Containers/Products/Product/Product.tsx
@@ -5,7 +5,9 @@ export default function Product(props: { imgPath: string; text: string }) {
   return (
     <div className="container">
       <div className="card product-card mb-4">
-        <img src={props.imgPath} className="card-img-top product-img" alt="" />
+        <div className="card-img-top product-img-content">
+          <img src={props.imgPath} className="product-img" alt="" />
+        </div>
         <div className="card-body product-card-content m-25">
           <p className="card-text">{props.text}</p>
         </div>
